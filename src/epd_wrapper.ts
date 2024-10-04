65559f867a50dea855d1bb137af0ee10af94c381
aslak <aslakrb@gmail.com>
2024-10-05 00:24:57 +0200

    add verbosity

--- src/epd_wrapper.ts
+++ src/epd_wrapper.ts
@@ -12,17 +12,22 @@ export function isAvailable() {
   return epdDriver !== null;
 }
 export function init() {
+  console.log("initialising epd");
   epdDriver?.init();
 }
 export function init4Gray() {
+  console.log("initialising 4gray mode");
   epdDriver?.init4Gray();
 }
 export function clear4Gray() {
+  console.log("clearing 4gray mode");
   epdDriver?.clear4Gray();
 }
 export function display4Gray(data: Uint8Array) {
+  console.log("displaying buffer on epd");
   epdDriver?.display4Gray(data);
 }
 export function sleep() {
+  console.log("putting display to sleep");
   epdDriver?.sleep();
 }
