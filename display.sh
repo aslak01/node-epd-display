e49b9912bede3f87e3b68a54970e2af538d18798
Aslak <hei@asla.cc>
2024-10-09 01:35:45 +0200

    try to not write date without a stderr

--- display.sh
+++ display.sh
@@ -1,6 +1,17 @@
 #!/bin/env bash
 source "$HOME/.bashrc"
+log_with_date() {
+  local log_file="$1"
+  local input
+  if read -t 1 input; then
+    {
+      echo "$input"
+      cat
+      date
+    } >>"$log_file"
+  fi
+}
 
 cd "$HOME/node-epd-display"
 
-npm run display 2>>"$HOME/node-epd-display/error.log" && date >>"$HOME/node-epd-display/error.log"
+npm run display 2> >(log_with_date "$HOME/node-epd-display/error.log")
